e82672880cd6b220d863b6ae834bd95cc4322e8e
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2015-10-10 17:44:31 -0500

    base de datos postgres en heroku

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,19 +4,19 @@
 # --- !Ups
 
 create table conductor (
-  id                        bigint auto_increment not null,
+  id                        bigserial not null,
   nombre                    varchar(255),
   cedula                    integer,
   celular                   integer,
   correo                    varchar(255),
-  desempenio                double,
+  desempenio                float,
   viajes_totales            integer,
   estado                    integer,
   constraint pk_conductor primary key (id))
 ;
 
 create table estacion_vcub (
-  id                        bigint auto_increment not null,
+  id                        bigserial not null,
   capacidad                 integer,
   vcubs                     integer,
   nombre                    varchar(255),
@@ -25,16 +25,16 @@ create table estacion_vcub (
 ;
 
 create table movibus (
-  id                        bigint auto_increment not null,
-  latitud                   double,
-  longitud                  double,
+  id                        bigserial not null,
+  latitud                   float,
+  longitud                  float,
   estado                    integer,
   kilometraje               integer,
   constraint pk_movibus primary key (id))
 ;
 
 create table pedido_movibus (
-  id                        bigint auto_increment not null,
+  id                        bigserial not null,
   fecha_pedido              timestamp,
   fecha_ejecucion           timestamp,
   tiempo_estimado           integer,
@@ -42,28 +42,28 @@ create table pedido_movibus (
   usuario_id                bigint,
   movibus_id                bigint,
   conductor_id              bigint,
-  latitud_usuario           double,
-  longitud_usuario          double,
-  latitud_destino           double,
-  longitud_destino          double,
+  latitud_usuario           float,
+  longitud_usuario          float,
+  latitud_destino           float,
+  longitud_destino          float,
   constraint pk_pedido_movibus primary key (id))
 ;
 
 create table pedido_movibus_pendiente (
-  id                        bigint auto_increment not null,
+  id                        bigserial not null,
   usuario_id                bigint,
   fecha_pedido              timestamp,
   fecha_ejecucion           timestamp,
-  latitud_usuario           double,
-  longitud_usuario          double,
-  latitud_destino           double,
-  longitud_destino          double,
+  latitud_usuario           float,
+  longitud_usuario          float,
+  latitud_destino           float,
+  longitud_destino          float,
   tiempo_estimado           integer,
   constraint pk_pedido_movibus_pendiente primary key (id))
 ;
 
 create table reporte (
-  id                        bigint auto_increment not null,
+  id                        bigserial not null,
   tipo_reporte              varchar(255),
   descripcion               varchar(255),
   tipo_accidente            integer,
@@ -72,9 +72,9 @@ create table reporte (
 ;
 
 create table tranvia (
-  id                        bigint auto_increment not null,
-  latitud                   double,
-  longitud                  double,
+  id                        bigserial not null,
+  latitud                   float,
+  longitud                  float,
   estado                    integer,
   kilometraje               integer,
   linea                     integer,
@@ -82,7 +82,7 @@ create table tranvia (
 ;
 
 create table usuario (
-  id                        bigint auto_increment not null,
+  id                        bigserial not null,
   nombre                    varchar(255),
   cedula                    integer,
   celular                   integer,
@@ -92,36 +92,32 @@ create table usuario (
   constraint pk_usuario primary key (id))
 ;
 
-alter table pedido_movibus add constraint fk_pedido_movibus_usuario_1 foreign key (usuario_id) references usuario (id) on delete restrict on update restrict;
+alter table pedido_movibus add constraint fk_pedido_movibus_usuario_1 foreign key (usuario_id) references usuario (id);
 create index ix_pedido_movibus_usuario_1 on pedido_movibus (usuario_id);
-alter table pedido_movibus add constraint fk_pedido_movibus_movibus_2 foreign key (movibus_id) references movibus (id) on delete restrict on update restrict;
+alter table pedido_movibus add constraint fk_pedido_movibus_movibus_2 foreign key (movibus_id) references movibus (id);
 create index ix_pedido_movibus_movibus_2 on pedido_movibus (movibus_id);
-alter table pedido_movibus add constraint fk_pedido_movibus_conductor_3 foreign key (conductor_id) references conductor (id) on delete restrict on update restrict;
+alter table pedido_movibus add constraint fk_pedido_movibus_conductor_3 foreign key (conductor_id) references conductor (id);
 create index ix_pedido_movibus_conductor_3 on pedido_movibus (conductor_id);
-alter table pedido_movibus_pendiente add constraint fk_pedido_movibus_pendiente_us_4 foreign key (usuario_id) references usuario (id) on delete restrict on update restrict;
+alter table pedido_movibus_pendiente add constraint fk_pedido_movibus_pendiente_us_4 foreign key (usuario_id) references usuario (id);
 create index ix_pedido_movibus_pendiente_us_4 on pedido_movibus_pendiente (usuario_id);
 
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+drop table if exists conductor cascade;
 
-drop table if exists conductor;
+drop table if exists estacion_vcub cascade;
 
-drop table if exists estacion_vcub;
+drop table if exists movibus cascade;
 
-drop table if exists movibus;
+drop table if exists pedido_movibus cascade;
 
-drop table if exists pedido_movibus;
+drop table if exists pedido_movibus_pendiente cascade;
 
-drop table if exists pedido_movibus_pendiente;
+drop table if exists reporte cascade;
 
-drop table if exists reporte;
+drop table if exists tranvia cascade;
 
-drop table if exists tranvia;
-
-drop table if exists usuario;
-
-SET REFERENTIAL_INTEGRITY TRUE;
+drop table if exists usuario cascade;
 
